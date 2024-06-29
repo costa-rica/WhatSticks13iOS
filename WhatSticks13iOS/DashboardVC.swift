8a810951265f31e89ec0fbfc3bb16759306b46f4
Nick Rodriguez <nrodrig1@gmail.com>
2024-06-29 13:36:39 +0200

    First commit Main.storyboard deleted; Tab Bar navigation coded with with nav bar icons;

--- WhatSticks13iOS/DashboardVC.swift
+++ WhatSticks13iOS/DashboardVC.swift
@@ -1,19 +1,20 @@
 //
-//  ViewController.swift
-//  WhatSticks13iOS
+//  DashboardVC.swift
+//  TabBar07
 //
 //  Created by Nick Rodriguez on 28/06/2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DashboardVC: TemplateVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+        self.lblScreenName.text = "Dashboard"
     }
 
 
 }
-
